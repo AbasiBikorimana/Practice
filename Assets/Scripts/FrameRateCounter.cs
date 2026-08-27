3daf7fac76a0957a002add5b8178e699ea137408
Abasi Bikorimana <121476273+AbasiBikorimana@users.noreply.github.com>
2026-08-27 11:59:31 -0500

    FPS Counter Start

---
+++ Assets/Scripts/FrameRateCounter.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+
+public class FrameRateCounter : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI display;
+
+    [SerializeField, Range(0.1f, 2f)]
+    float sampleDuration = 1f;
+
+    int frames;
+    float duration;
+
+    void Update()
+    {
+        float FrameDuration = Time.unscaledDeltaTime;
+        frames += 1;
+        duration += FrameDuration;
+        
+        if (duration >= sampleDuration)
+        {
+            display.SetText("FPS\n{0:0}\n000\n000", frames / duration);
+            frames = 0;
+            duration = 0f;
+        }
+
+    }
+}
