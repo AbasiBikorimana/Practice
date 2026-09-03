caeccedb1b4ec56fa6051f3a43ae266914245e8b
Abasi Bikorimana <121476273+AbasiBikorimana@users.noreply.github.com>
2026-09-02 23:30:54 -0500

    GPUGraph Implemented

--- Assets/Point/PointGPU.hlsl
+++ Assets/Point/PointGPU.hlsl
@@ -1,15 +1,38 @@
+#ifndef POINT_GPU_INCLUDED
+#define POINT_GPU_INCLUDED
+
+#pragma instancing_options assumeuniformscaling procedural:ConfigureProcedural
+#pragma editor_sync_compilation
+
 #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
-	StructuredBuffer<float3> _Positions;
+    StructuredBuffer<float3> _Positions;
 #endif
 
 float _Step;
 
-void ConfigureProcedural () {
-	#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
-		float3 position = _Positions[unity_InstanceID];
+void ConfigureProcedural()
+{
+    #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
 
-		unity_ObjectToWorld = 0.0;
-		unity_ObjectToWorld._m03_m13_m23_m33 = float4(position, 1.0);
-		unity_ObjectToWorld._m00_m11_m22 = _Step;
-	#endif
+        float3 position = _Positions[unity_InstanceID];
+
+        unity_ObjectToWorld = 0.0;
+        unity_ObjectToWorld._m03_m13_m23_m33 =
+            float4(position, 1.0);
+
+        unity_ObjectToWorld._m00_m11_m22 = _Step;
+
+    #endif
 }
+
+void ShaderGraphFunction_float(float3 In, out float3 Out)
+{
+    Out = In;
+}
+
+void ShaderGraphFunction_half(half3 In, out half3 Out)
+{
+    Out = In;
+}
+
+#endif
--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -9,20 +9,9 @@ public static class FunctionLibrary
 
     static Function[] functions = { Wave, MultiWave, Ripple, Sphere, Torus };
 
-    public static FunctionName GetNextFunctionName (FunctionName name)
-    {
-        return (int) name < functions.Length - 1 ? name + 1 : 0;
-        /*
-        if ((int)name < functions.Length - 1)
-        {
-            return name + 1;
-        }
-        else
-        {
-            return 0;
-        }
-        */
-    }
+    public static Function GetFunction(FunctionName name) => functions[(int)name];
+
+    public static FunctionName GetNextFunctionName (FunctionName name) => (int)name < functions.Length - 1 ? name + 1 : 0;
 
     public static FunctionName GetRandomFunctionName (FunctionName name)
     {
@@ -30,10 +19,8 @@ public static class FunctionLibrary
         return choice == name ? 0 : choice;
     }
 
-    public static Function GetFunction (FunctionName name)
-    {
-        return functions[(int)name];
-    }
+    public static int FunctionCount => functions.Length;
+
 
     public static Vector3 Wave (float u, float v, float t)
     {
